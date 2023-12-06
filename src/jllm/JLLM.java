4c07f183165c3d09883af69f0c7959189c7b16f7
F4CC3 <alfonso.cregcalv@gmail.com>
2023-12-06 16:27:39 +0100

    Comienzo del main

---
+++ src/jllm/JLLM.java
@@ -0,0 +1,13 @@
+package jllm;
+/*
+ * @author alfonso
+ */
+public class JLLM {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
